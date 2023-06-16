4a4d4a968fee263675473e2187c0ce03d21adac0
MatiSega <mattisegade@gmail.com>
2023-06-16 10:19:24 -0300

    tercero del dia

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ alert("Bienvenidos a Gancel");
 let nombre = prompt("Ingrese su nombre");
 
 // Variables de precios
-const PRECIO_KILO_ENVUELTO = 1400;
+const PRECIO_KILO_FRUTILLA = 1400;
 const PRECIO_CAJA_X40 = 1200;
 const PRECIO_KILO_SIN_ENVOLVER = 1100;
 const precios = [
-  { nombre: 'PRECIO_KILO_ENVUELTO', valor: PRECIO_KILO_ENVUELTO },
+  { nombre: 'PRECIO_KILO_FRUTILLA', valor: PRECIO_KILO_FRUTILLA },
   { nombre: 'PRECIO_CAJA_X40', valor: PRECIO_CAJA_X40 },
   { nombre: 'PRECIO_KILO_SIN_ENVOLVER', valor: PRECIO_KILO_SIN_ENVOLVER }
 ];
@@ -24,15 +24,15 @@ function calcularCostoTotal(cantidad, precio) {
 let seleccionValida = false;
 let seleccion;
 while (!seleccionValida) {
-  seleccion = parseInt(prompt("Seleccione 1 para Kilo Envuelto, 2 para Caja x40, 3 para Kilo sin envolver"));
+  seleccion = parseInt(prompt("Seleccione 1 para Kilo Frutilla, 2 para Caja x40, 3 para Kilo sin envolver"));
 
   let precioEncontrado; // Precio encontrado para el producto seleccionado
 
   switch (seleccion) {
     case 1:
       alert("Kilo Envuelto seleccionado");
-      precioEncontrado = precios.find(precio => precio.nombre === 'PRECIO_KILO_ENVUELTO').valor;
-      let cantidadKiloEnvuelto = parseInt(prompt("Ingrese la cantidad de kilos envueltos que desea"));
+      precioEncontrado = precios.find(precio => precio.nombre === 'PRECIO_KILO_FRUTILLA').valor;
+      let cantidadKiloEnvuelto = parseInt(prompt("Ingrese la cantidad de kilos que desea"));
       cantidadArticulos += cantidadKiloEnvuelto;
       costoTotal += calcularCostoTotal(cantidadKiloEnvuelto, precioEncontrado);
       seleccionValida = true;
