eda4df7aac7b4bee2efcd19abb4a9209b0f8b466
MatiSega <mattisegade@gmail.com>
2023-06-30 11:19:57 -0300

    agregandoProductos

--- script.js
+++ script.js
@@ -1,6 +1,116 @@
-const PRECIO_FRUTILLITAS = 1400;
-const PRECIO_CAJITA = 1200;
-const PRECIO_KILO = 1100;
+const productos = [
+      {
+
+        id: 1,
+        nombre: "Cajita",
+        imagen: "./img/cajita.png",
+        valor: "$1250",
+
+      },
+
+      {
+
+        id: 2,
+        nombre: "Kilo sin envolver",
+        imagen: "./img/kilossin.png",
+        valor: "$1100",
+
+      },
+
+      {
+
+        id: 3,
+        nombre: "Frutillitas",
+        imagen: "./img/frutillas.sueltas.webp.png",
+        valor: "$1150",
+
+      },
+
+      {
+
+        id: 4,
+        nombre: "Kilo bombom Manzana",
+        imagen: "./img/bombon.Manzana.jpg.png",
+        valor: "$1150",
+      },
+
+      {
+
+        id: 5,
+        nombre: "Kilo bombom Limon",
+        imagen: "./img/bombon.Limon.webp.png",
+        valor: "$1150",
+
+      },
+
+      {
+
+        id: 6,
+        nombre: "Kilo bombom Uva",
+        imagen: "./img/bombon.Uva.webp.png",
+        valor: "$1150",
+      },
+
+      {
+
+        id: 7,
+        nombre: " Kilo bombom frutilla",
+        imagen: "./img/bonbon.cereza.webp.png",
+        valor: "$1150",
+
+      },
+
+      {
+
+        id: 8,
+        nombre: "Kilo bombom Naranja",
+        imagen: "./img/bombon.Naranja.jpg.png",
+        valor: "$1150",
+
+      },
+
+      {
+
+        id: 9,
+        nombre: "Alfajor Don Salvador Negro",
+        imagen: "./img/alfajor-negro.webp.png",
+        valor: "$4500",
+
+      },
+
+      {
+        id: 10,
+        nombre: "Alfajor Don Salvador Blanco",
+        imagen: "./img/alfajor-blanco.webp.png",
+        valor: "$4500"
+
+      }
+
+]
+
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+productos.forEach((producto) => {
+  const card = document.createElement("div");
+  card.classList.add("col");
+  card.innerHTML = `
+    <div class="card h-100">
+      <img src="${producto.imagen}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <h5>${producto.valor}</h5>
+      </div>
+      <div class="d-grid gap-2">
+  <button class="btn btn-primary" type="button">Agregar al Carrito</button>
+</div>
+    </div>
+  `;
+  contenedorProductos.appendChild(card);
+});
+
+
+
+console.log(productos)
 
 let cantidadArticulos = 0;
 let costoTotal = 0;
